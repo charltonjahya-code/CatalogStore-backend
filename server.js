cb3ccfed97f9620c5f5833b7b6ca4b8989806620
ChaltonMillener <cmillener@student.unimelb.edu.au>
2026-07-13 16:22:29 +0700

    add GET /products route reading from database

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const db = require('./models');
 
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello World! The server is alive.')
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    // 1. fetch all products (await db.Product.findAll())
+    const products = await db.Product.findAll();
+    // 2. send them back (res.json(...))
+    res.json(products);
+  } catch (error) {
+    // send an error: res.status(500).json({ error: 'something' })
+    res.status(500).json({error: 'server error'});
+  }
 });
 
 app.listen(port, () => {
